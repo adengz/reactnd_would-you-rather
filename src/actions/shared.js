8048e24780813e44ab36f36f0537416f55b4d79c
adengz <adengz@gmail.com>
2020-07-08 15:18:59 -0700

    feat: Force log out before loading initial data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,12 @@
 import { _getUsers, _getQuestions } from '../utils/_DATA'
+import { logOut } from './authedUser';
 import { receiveUsers } from './users';
 import { receiveQuestions } from './questions';
 
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(logOut());
+    
     return Promise.all([
       _getUsers(),
       _getQuestions()
