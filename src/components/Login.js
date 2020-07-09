8788da98b31b333878b228f9814d6c58e18b023a
adengz <adengz@gmail.com>
2020-07-08 18:17:55 -0700

    feat: Implement login logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,25 @@ import { connect } from 'react-redux';
 import { logIn } from '../actions/authedUser';
 
 class Login extends React.Component {
+  state = {
+    selectedUserId: ''
+  };
+
+  handleSelectUser = (e) => {
+    const selectedUserId = e.target.value;
+    this.setState({
+      selectedUserId
+    });
+  }
+
   handleSignIn = (e) => {
     e.preventDefault();
 
-    // TODO: implement sign in action
+    const userId = this.state.selectedUserId;
+
+    userId === ''
+      ? alert('Select your name to sign in')
+      : this.props.dispatch(logIn(userId));
   }
 
   render() {
@@ -16,7 +31,7 @@ class Login extends React.Component {
       <div className="login">
         <h1>Would you rather...</h1>
         <p>Sign in to continue</p>
-        <select value={''}>
+        <select className="user-select" value={''} onChange={this.handleSelectUser}>
           <option value={''} disabled>Select User</option>
           {Object.keys(users).map((id) => (
             <option key={id} value={id}>{users[id].name}</option>
